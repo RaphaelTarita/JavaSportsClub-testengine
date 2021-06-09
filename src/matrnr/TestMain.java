17e9f4a389c9d90b9d9d79e425dfd6a263a5396d
Raphael Tarita <raphael.tarita@gmail.com>
2021-06-09 17:05:18 +0200

    clear TestMain

--- src/matrnr/TestMain.java
+++ src/matrnr/TestMain.java
@@ -6,15 +6,5 @@ import java.nio.file.Path;
 
 public class TestMain {
     public static void main(String[] args) {
-        TestEngine engine = TestEngine.builder()
-            .registerSuite(Suites.STRUCTURE_CHECKS)
-            .registerSuite(Suites.NULL_PARAMETER_CHECKS)
-            .registerSuite(Suites.NULL_RETURN_CHECKS)
-            .registerSuite(Suites.CONTRACT_CHECKS)
-            .registerSuite(Suites.SHALLOW_COPY_CHECKS)
-            .fullOutput(Path.of("C:\\Users\\rapha\\Desktop\\testresults.txt"))
-            .build();
-
-        engine.test();
     }
 }
